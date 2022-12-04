19fc673ba580e32e825867742a7156a5d081a3f0
Kamil Bochno <bochno.kamil@gmail.com>
2022-12-04 19:56:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import Navbar from './Components/Navbar/Navbar';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar></Navbar>
+    </div>
+  );
+}
+
+export default App;
