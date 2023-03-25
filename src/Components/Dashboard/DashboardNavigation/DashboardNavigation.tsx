63a25e86f007a7931045fdfb4a4701fc37c663db
Kamil <bochno.kamil@gmail.com>
2023-03-25 18:23:58 +0100

    Added dashboard navigation component

---
+++ src/Components/Dashboard/DashboardNavigation/DashboardNavigation.tsx
@@ -0,0 +1,206 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+import Footer from "../Footer/Footer.tsx";
+import { useState, useEffect } from "react";
+import {
+  NavLink,
+  Routes,
+  Route,
+  useParams,
+  Router,
+  useLocation,
+  useNavigate
+} from "react-router-dom";
+import { Container } from "@mui/system";
+import routes from "../routes/menu.tsx";
+import { Fragment } from "react";
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { Bars3Icon, BellIcon, XMarkIcon } from "@heroicons/react/24/outline";
+import logo from "../../../Icons/mdi_car-wrench.svg";
+
+function DashboardNavigation() {
+  const navigate = useNavigate();
+
+  const user = {
+    name: "Tom Cook",
+    email: "tom@example.com",
+    imageUrl:
+      "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+  };
+  const navigation = [
+    { name: "Dashboard", href: "" },
+    { name: "My cars", href: "cars" },
+    { name: "Appointment", href: "appointment" },
+    { name: "Shop", href: "shop" }
+  ];
+  const userNavigation = [
+    { name: "Your Profile", href: "#" },
+    { name: "Settings", href: "#" },
+    { name: "Sign out", href: "#" }
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
+  const logout = () => {
+    const token = localStorage.getItem("token");
+    localStorage.clear();
+    navigate("/");
+  };
+
+  return (
+    <div className="min-h-full">
+      <Disclosure as="div" className="w-full bg-gray-800">
+        {({ open }) => (
+          <>
+            <div className="mx-auto max-w-7xl px-6 sm:px-6 lg:px-8">
+              <div className="relative flex h-16 items-center justify-between">
+                <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Open main menu</span>
+                    {open ? (
+                      <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
+                    ) : (
+                      <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+                  <div className="flex flex-shrink-0 items-center">
+                    <img
+                      className="block h-8 text-white bg-blue-200 rounded-full w-auto lg:hidden"
+                      src={logo}
+                      alt="AutoFixSolutions"
+                    />
+                    <img
+                      className="hidden h-8 text-white bg-blue-200 rounded-full w-auto lg:block"
+                      src={logo}
+                      alt="AutoFixSolutions"
+                    />
+                  </div>
+                  <div className="hidden sm:ml-6 sm:block mt-1">
+                    <div className="flex space-x-4">
+                      {navigation.map((item) => (
+                        <NavLink key={item.name} to={`/dashboard/${item.href}`}>
+                          {({ isActive }) => (
+                            <span
+                              className={classNames(
+                                isActive
+                                  ? "bg-gray-600 text-white"
+                                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                                "px-3 py-2 rounded-md text-sm font-medium"
+                              )}>
+                              {item.name}
+                            </span>
+                          )}
+                        </NavLink>
+                      ))}
+                      <NavLink to="/">
+                        <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                          Return to homepage
+                        </span>
+                      </NavLink>
+                    </div>
+                  </div>
+                </div>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                  <Menu as="div" className="relative ml-3">
+                    <div>
+                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                        <span className="sr-only">Open user menu</span>
+                        <img
+                          className="h-8 w-8 rounded-full"
+                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                          alt=""
+                        />
+                      </Menu.Button>
+                    </div>
+                    <Transition
+                      as={Fragment}
+                      enter="transition ease-out duration-100"
+                      enterFrom="transform opacity-0 scale-95"
+                      enterTo="transform opacity-100 scale-100"
+                      leave="transition ease-in duration-75"
+                      leaveFrom="transform opacity-100 scale-100"
+                      leaveTo="transform opacity-0 scale-95">
+                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                        <Menu.Item>
+                          {({ active }) => (
+                            <NavLink
+                              to={"/dashboard/profile"}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}>
+                              Your Profile
+                            </NavLink>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <NavLink
+                              to={"/dashboard/profile/settings"}
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}>
+                              Settings
+                            </NavLink>
+                          )}
+                        </Menu.Item>
+                        <Menu.Item>
+                          {({ active }) => (
+                            <a
+                              href="#"
+                              className={classNames(
+                                active ? "bg-gray-100" : "",
+                                "block px-4 py-2 text-sm text-gray-700"
+                              )}>
+                              Sign out
+                            </a>
+                          )}
+                        </Menu.Item>
+                      </Menu.Items>
+                    </Transition>
+                  </Menu>
+                </div>
+              </div>
+            </div>
+
+            <Disclosure.Panel className="sm:hidden">
+              <div className="space-y-1 px-2 pt-2 pb-3">
+                {navigation.map((item) => (
+                  <Disclosure.Button>
+                    <NavLink key={item.name} to={`/dashboard/${item.href}`}>
+                      {({ isActive }) => (
+                        <span
+                          className={classNames(
+                            isActive
+                              ? "bg-gray-600 text-white"
+                              : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                            "px-3 py-2 rounded-md text-sm font-medium"
+                          )}>
+                          {item.name}
+                        </span>
+                      )}
+                    </NavLink>
+                  </Disclosure.Button>
+                ))}
+                <Disclosure.Button>
+                  <NavLink to="/">
+                    <span className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                      Return to homepage
+                    </span>
+                  </NavLink>
+                </Disclosure.Button>
+              </div>
+            </Disclosure.Panel>
+          </>
+        )}
+      </Disclosure>
+    </div>
+  );
+}
+
+export default DashboardNavigation;
