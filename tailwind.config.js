af5de09b2f6e3dae83ea2d38f36eab4c864f1b53
Kamil <bochno.kamil@gmail.com>
2023-03-26 21:55:09 +0200

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        98: "28rem",
+        100: "36rem"
+      }
+    }
   },
-  plugins: [],
-}
+  plugins: []
+};
