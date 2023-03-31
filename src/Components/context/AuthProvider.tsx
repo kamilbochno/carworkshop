c320bb081fd09dfdb61e75d720b525bcd01fa63c
Kamil <bochno.kamil@gmail.com>
2023-03-31 23:23:35 +0200

    Added logout and isAuthenticated functions

--- src/Components/context/AuthProvider.tsx
+++ src/Components/context/AuthProvider.tsx
@@ -1,21 +1,46 @@
-import React from 'react';
-import { createContext, useState } from 'react';
+import React from "react";
+import { createContext, useState } from "react";
+import axios from "axios";
+import { toast } from "react-hot-toast";
 
 const AuthContext = createContext({});
 
 export const AuthProvider = ({ children }) => {
-    const [auth, setAuth] = useState(() => {
-        if (localStorage.getItem('token')) {
-            return localStorage.getItem('token')
-        }
-        return null;
-    });
+  const [auth, setAuth] = useState(false);
+  const [admin, setAdmin] = useState(false);
 
-    return (
-        <AuthContext.Provider value={{ auth, setAuth }}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  const isAuthenticated = () => {
+    axios.post("/refresh").then((response) => {
+      if (response.status === 200) {
+        if (response.data.admin === true) {
+          setAdmin(true);
+        } else {
+          setAdmin(false);
+        }
+        setAuth(true);
+      } else {
+        setAuth(false);
+      }
+    });
+  };
+
+  const logout = () => {
+    axios
+      .get("/logout")
+      .then((res) => {
+        setAuth(false);
+        toast.success(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <AuthContext.Provider value={{ auth, setAuth, isAuthenticated, logout, admin, setAdmin }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
 
 export default AuthContext;
