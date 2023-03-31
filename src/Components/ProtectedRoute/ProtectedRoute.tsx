cd797b84d0608dcdcddd510496db05c15226c3c4
Kamil <bochno.kamil@gmail.com>
2023-03-31 21:59:52 +0200

    Added checking if user is authenticated

--- src/Components/ProtectedRoute/ProtectedRoute.tsx
+++ src/Components/ProtectedRoute/ProtectedRoute.tsx
@@ -1,22 +1,41 @@
-import React from "react";
-import { useContext } from "react";
-import { Navigate, useNavigate, Outlet } from "react-router-dom";
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../context/AuthProvider.tsx";
 
 export const ClientProtectedRoute = ({ children }) => {
-  const { auth, isAdmin } = useContext<any>(AuthContext);
+  const { auth, admin, isAuthenticated } = useContext<any>(AuthContext);
   const navigate = useNavigate();
-  return auth && !isAdmin ? children : navigate("/");
+  useEffect(() => {
+    isAuthenticated();
+  }, []);
+  if (auth && !admin) {
+    return children;
+  } else {
+    navigate("/");
+  }
 };
-
 export const AdminProtectedRoute = ({ children }) => {
-  const { auth, isAdmin } = useContext<any>(AuthContext);
+  const { auth, admin, isAuthenticated } = useContext<any>(AuthContext);
   const navigate = useNavigate();
-  return auth && isAdmin ? children : navigate("/");
+  useEffect(() => {
+    isAuthenticated();
+  }, []);
+  if (auth && admin) {
+    return children;
+  } else {
+    navigate("/");
+  }
 };
 
 export const ProtectedRoute = ({ children }) => {
-  const { auth } = useContext<any>(AuthContext);
+  const { auth, isAuthenticated } = useContext<any>(AuthContext);
   const navigate = useNavigate();
-  return !auth ? children : navigate("/");
+  useEffect(() => {
+    isAuthenticated();
+  }, []);
+  if (!auth) {
+    return children;
+  } else {
+    navigate("/");
+  }
 };
